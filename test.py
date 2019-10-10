cc48792b5686f304f66001a8b954cc9920210e9f
Charlie Minns <charlieminns@nat-oitwireless-inside-vapornet100-10-9-51-140.princeton.edu>
2019-10-10 15:24:22 -0400

    initial commit

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from scipy import stats
 from sklearn.cluster import KMeans, MeanShift
 from scipy.sparse.csgraph import minimum_spanning_tree
 from sklearn.neighbors import kneighbors_graph
@@ -16,7 +17,7 @@ class Edge():
         self.visited = False 
 
 # running Prim's algorithm to find the mst in a graph G with V vertices
-def prims(G, V):
+def prims(G, V, mean, mad, threshold):
     mst = []                            # list of edges in the mst
     one = []
     two = []
@@ -24,6 +25,8 @@ def prims(G, V):
     v = 0                               # starting algorithm from a random vertex
     visited = []                        # keeping track of the vertices that have been visited
     distances = []
+    n_clusters = 1
+    clusters = {}
 
     # go through vertices until an mst is found
     while(len(mst) != V - 1):
@@ -32,7 +35,7 @@ def prims(G, V):
 
         # adding edges from the current vertex
         for i in range(V):
-            if (G[v, i] != 0):
+            if (G[v, i] != 0 and G[v, i] < mean + threshold*mad):
                 edges.append(Edge(v, i, G[v, i]))
 
         # checking for the smallest edge from the visited vertices to a non-visited vertex
@@ -42,11 +45,13 @@ def prims(G, V):
 
         # remove smallest edge from potential edges and add it to mst
         if (smallest.weight == np.inf):
+            n_clusters += 1
             v = 1
             while (v in visited):
                 v += 1
                 if (v == V):
-                    return mst, one, two
+                    n_clusters -= 1
+                    return mst, one, two, distances, n_clusters
         else:
             edges.remove(smallest)
             mst.append(smallest)
@@ -55,7 +60,7 @@ def prims(G, V):
             distances.append(smallest.weight)
             v = smallest.vTo
 
-    return mst, one, two
+    return mst, one, two, distances, n_clusters
             
 # load the data
 data = np.load('clusters.npy')
@@ -69,13 +74,35 @@ X = np.dstack(((ra0-data['RA'])*np.cos(data['DEC']*np.pi/180), data['DEC']-dec0)
 # graph as a matrix: G[i, j] is the weight of the distance from i to j
 G = kneighbors_graph(X, n_neighbors=100, mode='distance')
 
-# run Prim's algorithm
-mst, one, two = prims(G, V)
+lengths = []
+for i in range(V):
+    for j in range(i, V):
+        l = G[i, j]
+        if (l != 0):
+            lengths.append(l)
+mean = np.mean(lengths)
+mad = stats.median_absolute_deviation(lengths)
+threshold = -1.2
 
-# plotting
-fig = plt.figure()
-ax = fig.add_subplot(111, aspect='equal')
+# run Prim's algorithm to find clusters
+mst, one, two, distances, n_clusters = prims(G, V, mean, mad, threshold)
+print(n_clusters)
+
+# plotting mst
+fig1 = plt.figure()
+ax = fig1.add_subplot(111, aspect='equal')
 ax.scatter(X[:,0], X[:,1], alpha=0.1)
 plt.quiver(X[one,0], X[one,1], X[two,0]-X[one,0], X[two,1]-X[one,1], angles='xy', scale_units='xy', scale=1, headwidth=0, headaxislength=0, headlength=0, minlength=0)
-fig.tight_layout()
+fig1.tight_layout()
+
+# plotting distances
+fig2 = plt.figure()
+ax_dist = fig2.add_subplot(111)
+x = np.linspace(1, len(distances), len(distances))
+ax_dist.plot(x, distances)
+ax_dist.set_title("Length of Edges Added")
+ax_dist.set_ylabel("Length of added edge")
+ax_dist.set_xlabel("Order of edges added")
+ax_dist.text(0, 0.8, "Threshold: " + str(threshold) + "\nNumber of Clusters: " + str(n_clusters), transform=ax.transAxes)
+
 plt.show()
